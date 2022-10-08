07b68246654113723aab96f672650609c0876dea
Yury Kusoits <kusoitsyury@gmail.com>
2022-10-08 20:03:25 +0200

    add task 1 hw 9

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+let intervalId = null;
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
 
+refs.startBtn.addEventListener('click', onChangeColor);
+refs.stopBtn.addEventListener('click', onStopChangeColor);
+
+function onChangeColor() {
+  intervalId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  refs.startBtn.setAttribute('disabled', true);
+}
+
+function onStopChangeColor() {
+  clearInterval(intervalId);
+  refs.startBtn.removeAttribute('disabled', true);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
